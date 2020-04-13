e63f84346ff0ed9903332d6055f78e69834943f4
davibaltar <davibaltarx@gmail.com>
2020-04-13 01:14:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# example-swagger-autogen
+
+Run the project and access the documentation at:
+
+[http://localhost:3000/doc](http://localhost:3000/doc)
+
+## Screenshots
+See the result after construction in the image below:
+
+![](https://raw.githubusercontent.com/davibaltar/public-store/master/screen-swagger-autogen-small.png)
+
+---
+
+![](https://raw.githubusercontent.com/davibaltar/public-store/master/screen-swagger-autogen-cut.png)
+
+## License
+[MIT] (LICENSE) License
---
+++ src/users.js
@@ -0,0 +1,32 @@
+
+function addUser(param) {
+    return true
+}
+
+function getUser(param) {
+    const user = {
+        name: "Lewis Doe",
+        age: 32,
+        parents: {
+            father: "Simon Doe",
+            mother: "Marie Doe"
+        },
+        diplomas: [
+            {
+                school: "ABC University",
+                year: 2020,
+                completed: true,
+                internship: {
+                    hours: 310,
+                    location: "ABC Company"
+                }
+            }
+        ]
+    }
+    return user
+}
+
+module.exports = {
+    addUser,
+    getUser
+}
