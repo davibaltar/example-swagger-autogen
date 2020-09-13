0cb24ffc8fc51a6adae5593207a1cd893a9a5a6a
davibaltar <davibaltarx@gmail.com>
2020-09-13 18:21:48 -0300

    Refactoring

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/*
+ * Run the project and access the documentation at: http://localhost:3000/doc
+ *
+ * Use the command below to generate the documentation without starting the project:
+ * $ npm start
+ *
+ * Use the command below to generate the documentation at project startup:
+ * $ npm run swagger-autogen
+ */
+
 const app = require('express')()
 const http = require('http')
 const swaggerUi = require('swagger-ui-express')
@@ -7,7 +17,7 @@ const bodyParser = require('body-parser')
 /* Server */
 const httpPort = 3000
 http.createServer(app).listen(httpPort)
-console.log("Listening at:// port:%s (HTTP)", httpPort)
+console.log("Access the link: http://localhost:%s/doc", httpPort)
 
 /* Middlewares */
 app.use(bodyParser.json())
